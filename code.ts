8dce647d8fa50da50068af52c08e433dc074ee77
Hazem Abdennadher <Hazem0Abdennadher@gmail.com>
2023-04-07 18:29:20 +0100

    auto generation from static input

--- code.ts
+++ code.ts
@@ -5,27 +5,30 @@ figma.ui.onmessage = (msg) => {
   if (msg.type === 'actionGenerate') {
     // current page
     const currentPage = figma.currentPage;
-    const DESIGN = figma.createFrame();
-
-    DESIGN.name = 'my design';
-    DESIGN.layoutMode = 'VERTICAL';
-    DESIGN.counterAxisSizingMode = 'AUTO';
-    DESIGN.primaryAxisSizingMode = 'AUTO';
     // create random design
-    components.forEach((comp) => {
-      // select random child and added to design
-      const selectedComp = figma.root.children.find(
-        (child) => child.id === comp.id
-      );
-      if (!selectedComp) return;
-      const randomChild =
-        selectedComp.children[
-          Math.floor(Math.random() * selectedComp.children.length)
-        ].clone();
-      DESIGN.appendChild(randomChild);
+    data.forEach((variation) => {
+      // create variation frame
+      const VARIATION = figma.createFrame();
+      VARIATION.name = variation.variation;
+      VARIATION.layoutMode = 'VERTICAL';
+      VARIATION.counterAxisSizingMode = 'AUTO';
+      VARIATION.primaryAxisSizingMode = 'AUTO';
+      VARIATION.clipsContent = false;
+      VARIATION.fills = [{ type: 'SOLID', color: { r: 0.8, g: 0.8, b: 0.8 } }];
+      // select components
+      variation.components.forEach((component) => {
+        const COMPONENT = figma.getNodeById(component[2]);
+        const COMPONENT_INSTANCE = COMPONENT?.parent?.children?.find(
+          (child) => child.id === component[2]
+        );
+        if (!COMPONENT_INSTANCE) return;
+        // create component instance
+        console.log(COMPONENT_INSTANCE);
+        const COMPONENT_INSTANCE_CLONE = COMPONENT_INSTANCE.clone();
+        COMPONENT_INSTANCE_CLONE.name = component[0];
+        VARIATION.appendChild(COMPONENT_INSTANCE_CLONE);
+      });
     });
-
-    currentPage.appendChild(DESIGN);
   }
   if (msg.type === 'actionExit') {
     figma.closePlugin();
@@ -33,29 +36,17 @@ figma.ui.onmessage = (msg) => {
   figma.closePlugin();
 };
 
-const components = [
+const data = [
   {
-    name: 'navbar',
-    id: '129:13658',
-  },
-  {
-    name: 'hero header',
-    id: '519:5759',
-  },
-  {
-    name: 'header',
-    id: '519:5701',
-  },
-  {
-    name: 'Features',
-    id: '507:5360',
-  },
-  {
-    name: 'Gallery',
-    id: '1161:5816',
-  },
-  {
-    name: 'footer',
-    id: '136:5239',
+    variation: 'Variation 1',
+    components: [
+      ['Navbar', '4', '405:9218'],
+      ['Hero Header', '76', '2110:41'],
+      ['Features', '353', '2021:53'],
+      ['Testimonials', '32', '2021:395'],
+      ['Pricing', '27', '712:23341'],
+      ['CTA', '38', '2110:247'],
+      ['Footer', '8', '2025:799'],
+    ],
   },
 ];
